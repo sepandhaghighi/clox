70724495419ba446ecae8507508781cf51937957
sepandhaghighi <sepand.haghighi@yahoo.com>
2024-11-23 20:26:19 +0330

    feat : clear_screen function added

--- clox/__main__.py
+++ clox/__main__.py
@@ -9,7 +9,17 @@ def main():
 
     :return: None
     """
-    pass
+    parser = argparse.ArgumentParser()
+    parser.epilog = ADDITIONAL_INFO
+    parser.add_argument('--timezone', help='timezone', type=str, choices=TIMEZONE_LIST)
+    parser.add_argument('--v-shift', help='vertical shift', type=int)
+    parser.add_argument('--h-shift', help='horizontal shift', type=int)
+    parser.add_argument('--version', help='version', nargs="?", const=1)
+    args = parser.parse_args()
+    try:
+        run_clock(args)
+    except (KeyboardInterrupt, EOFError):
+        print(EXIT_MESSAGE)
 
 
 if __name__ == "__main__":
--- clox/functions.py
+++ clox/functions.py
@@ -1,2 +1,15 @@
 # -*- coding: utf-8 -*-
 """clox functions."""
+import os
+import sys
+
+def clear_screen():
+    """
+    Clear screen function.
+
+    :return: None
+    """
+    if sys.platform == "win32":
+        os.system('cls')
+    else:
+        os.system('clear')
