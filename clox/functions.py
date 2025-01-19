ebe1f2105a30e333cc9d8b496cf9be7d1061677d
sepandhaghighi <sepand.haghighi@yahoo.com>
2025-01-19 13:47:04 +0330

    fix : autopep8

--- clox/functions.py
+++ clox/functions.py
@@ -84,7 +84,15 @@ def show_timezones_list():
         print("{0}. {1}".format(index, timezone))
 
 
-def run_clock(timezone=None, v_shift=0, h_shift=0, face=1, no_blink=False, vertical=False, hide_date=False, hide_timezone=False):
+def run_clock(
+        timezone=None,
+        v_shift=0,
+        h_shift=0,
+        face=1,
+        no_blink=False,
+        vertical=False,
+        hide_date=False,
+        hide_timezone=False):
     """
     Run clock.
 
