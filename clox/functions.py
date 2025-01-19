9062762d13c36068bc40599f8488a0f5c54ec06f
sepandhaghighi <sepand.haghighi@yahoo.com>
2025-01-19 13:43:44 +0330

    feat : --hide-timezone argument added

--- clox/functions.py
+++ clox/functions.py
@@ -154,6 +154,7 @@ def main():
     parser.add_argument('--no-blink', help='disable blinking mode', nargs="?", const=1)
     parser.add_argument('--vertical', help='vertical mode', nargs="?", const=1)
     parser.add_argument('--hide-date', help='hide date', nargs="?", const=1)
+    parser.add_argument('--hide-timezone', help='hide timezone', nargs="?", const=1)
     args = parser.parse_args()
     if args.version:
         print(CLOX_VERSION)
