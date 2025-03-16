dbb1f404eaf73e8b474723c3b81d764e9bb81d05
Sepand Haghighi <sepand.haghighi@yahoo.com>
2025-03-16 04:20:57 +0330

    Version 0.8 (#44)
    * rel : migrate to version 0.8
    
    * rel : CHANGELOG.md updated

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ def read_description():
 setup(
     name='clox',
     packages=['clox'],
-    version='0.7',
+    version='0.8',
     description='A Geeky Clock for Terminal Enthusiasts',
     long_description=read_description(),
     long_description_content_type='text/markdown',
@@ -37,7 +37,7 @@ setup(
     author='Sepand Haghighi',
     author_email='me@sepand.tech',
     url='https://github.com/sepandhaghighi/clox',
-    download_url='https://github.com/sepandhaghighi/clox/tarball/v0.7',
+    download_url='https://github.com/sepandhaghighi/clox/tarball/v0.8',
     keywords="clock time timer timezone terminal cli geek clox",
     project_urls={
         'Source': 'https://github.com/sepandhaghighi/clox'
