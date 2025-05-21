549e5209d677e1a4b6817ede589bb7241540d2a7
sepandhaghighi <sepand.haghighi@yahoo.com>
2025-05-21 18:45:29 +0330

    fix : show_date_formats_list function modified

--- clox/functions.py
+++ clox/functions.py
@@ -138,7 +138,7 @@ def show_date_formats_list(date_system: str = "GREGORIAN") -> None:
         datetime_lib = jdatetime
         example_year = 1368
     print("Date formats list:\n")
-    example_date = datetime_lib.datetime(year=example_year, month=1, day=1)
+    example_date = datetime_lib.datetime(year=example_year, month=7, day=23)
     for index, date_format in enumerate(DATE_FORMATS_LIST, 1):
         print("{index}. {date_format_code} - {date_format_example}".format(index=index,
                                                                            date_format_code=date_format, date_format_example=example_date.strftime(DATE_FORMATS_MAP[date_format])))
