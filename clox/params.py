8288a96d44085b4c132a2ce2539ac45843637368
Sepand Haghighi <sepand.haghighi@yahoo.com>
2025-05-22 20:43:50 +0330

    Version 1.1 (#64)
    * rel : migrate to version 1.1
    
    * rel : CHANGELOG.md updated

--- clox/params.py
+++ clox/params.py
@@ -2,7 +2,7 @@
 """clox params."""
 import pytz
 
-CLOX_VERSION = "1.0"
+CLOX_VERSION = "1.1"
 
 CLOX_OVERVIEW = '''
 Clox is a terminal-based clock application designed for terminal enthusiasts who appreciate simplicity,
