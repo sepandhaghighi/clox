a3509bae3facef47d8efaa2ec05ff608eb073046
sepandhaghighi <sepand.haghighi@yahoo.com>
2025-08-05 02:13:15 +0330

    fix : minor edit in offset show

--- clox/functions.py
+++ clox/functions.py
@@ -291,14 +291,14 @@ def run_clock(
             print(" " * h_shift, end='')
             print("Timezone: {timezone}".format(timezone=timezone_str))
             if offset_timezone != 0:
-                print("Timezone Offset: {offset}h".format(offset=round(offset_timezone, 2)))
+                print("Timezone Offset: {offset}h".format(offset=offset_timezone))
             if timezone is not None:
                 datetime_local = datetime.datetime.now() + datetime.timedelta(hours=offset_local)
                 time_local_str = datetime_local.strftime(time_formats_local[format_index])
                 print(" " * h_shift, end='')
                 print("Local Time: {local_time}".format(local_time=time_local_str))
                 if offset_local != 0:
-                    print("Local Offset: {offset}h".format(offset=round(offset_local, 2)))
+                    print("Local Offset: {offset}h".format(offset=offset_local))
         time.sleep(1)
         if not no_blink:
             format_index = int(not format_index)
