9d7f47578111c524ef0560ad24e591c16c6d033d
Sepand Haghighi <sepand.haghighi@yahoo.com>
2025-08-22 05:04:50 +0330

    Offset Bug (#69)
    * fix : offset_local printing fixed
    
    * fix : print_calendar function modified
    
    * fix : h_shift bug fixed
    
    * fix : h_shift bug fixed
    
    * fix : OFFSET_FORMAT updated
    
    * fix : test.yml updated
    
    * doc : CHANGELOG.md updated

--- clox/functions.py
+++ clox/functions.py
@@ -215,7 +215,14 @@ def print_calendar(
     print(" " * h_shift, end='')
     print("Today: {date}".format(date=date_timezone_str))
     print(" " * h_shift, end='')
-    print("Timezone: {timezone}\n".format(timezone=timezone_str))
+    print("Timezone: {timezone}".format(timezone=timezone_str))
+    if offset_timezone != 0:
+        print(" " * h_shift, end='')
+        print(OFFSET_FORMAT.format(offset_type="Timezone", offset_value=offset_timezone))
+    if offset_local != 0:
+        print(" " * h_shift, end='')
+        print(OFFSET_FORMAT.format(offset_type="Local", offset_value=offset_local))
+    print("")
     calendar_str = calendar_obj.formatmonth(datetime_timezone.year, datetime_timezone.month)
     if mode == "YEAR":
         calendar_str = calendar_obj.formatyear(datetime_timezone.year)
@@ -296,14 +303,16 @@ def run_clock(
             print(" " * h_shift, end='')
             print("Timezone: {timezone}".format(timezone=timezone_str))
             if offset_timezone != 0:
+                print(" " * h_shift, end='')
                 print(OFFSET_FORMAT.format(offset_type="Timezone", offset_value=offset_timezone))
             if timezone is not None:
                 datetime_local = datetime.datetime.now() + offset_local_timedelta
                 time_local_str = datetime_local.strftime(time_formats_local[format_index])
                 print(" " * h_shift, end='')
                 print("Local Time: {local_time}".format(local_time=time_local_str))
-                if offset_local != 0:
-                    print(OFFSET_FORMAT.format(offset_type="Local", offset_value=offset_local))
+            if offset_local != 0:
+                print(" " * h_shift, end='')
+                print(OFFSET_FORMAT.format(offset_type="Local", offset_value=offset_local))
         time.sleep(1)
         if not no_blink:
             format_index = int(not format_index)
