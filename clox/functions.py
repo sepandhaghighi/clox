d7a1e8a31e7092625e4c08c5331f87a486db4aeb
sepandhaghighi <sepand.haghighi@yahoo.com>
2026-08-31 14:18:08 +0330

    feat : add --12h flag

--- clox/functions.py
+++ clox/functions.py
@@ -420,7 +420,7 @@ def main() -> None:
     parser.add_argument('--vertical', help='vertical mode', nargs="?", const=1)
     parser.add_argument('--hide-date', help='hide date', nargs="?", const=1)
     parser.add_argument('--hide-timezone', help='hide timezone', nargs="?", const=1)
-    parser.add_argument('--am-pm', help='AM/PM mode', nargs="?", const=1)
+    parser.add_argument('--12h', '--am-pm', dest='am_pm', help='use 12-hour time format', nargs='?', const=1)
     parser.add_argument('--once', help='print current time once and exit immediately', nargs='?', const=1)
     parser.add_argument('--calendar', help='calendar mode', type=str.upper, choices=CALENDARS_LIST)
     parser.add_argument('--first-weekday', help='first weekday', type=str.upper, default="MONDAY",
